93a6da8d3dd3ae3e19fdd69d5990fc272fc3f0c4
Aymen <aymenmaddouritunisie@gmail.com>
2018-08-06 22:28:09 +0100

    first commit

---
+++ HelloProgramme/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloProgramme
+{
+  public  class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(afficherMessage());
+        }
+        public static string afficherMessage(){
+
+            return "Bonjour et bonne journée";
+        }
+    }
+}
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloProgramme;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Bonjour et bonne journée", Program.afficherMessage());
+        }
+    }
+}
